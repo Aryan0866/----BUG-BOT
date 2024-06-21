9973d86e453a6666656ee069a0dddf783b2186f7
Aryan0866 <140366478+Aryan0866@users.noreply.github.com>
2024-06-21 10:13:57 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-//base by Tohid-khan
+//base by .𝛥𝑅𝑌𝛥𝛮-𝑅𝛥𝐽𝛲𝛩𝛩𝑇
 //re-upload? recode? copy code? give credit ya :)
-//YouTube: @tohidkhan_6332
-//Instagram: tohidkhan6332
-//Telegram: t.me/@tohid_mewati
-//GitHub: @tohidkhan6332
-//WhatsApp: +917849917350
-//want more free bot scripts? subscribe to my youtube channel: https://youtube.com/@tohidkhan_6332
+//YouTube: @aryanedit3688
+//Instagram: aryanrajpoot_375
+//Tiktok: aryanrajpoot375
+//GitHub: @Aryan0866
+//WhatsApp: +923187842447
+//want more free bot scripts? subscribe to my youtube channel: https://youtube.com/@aryanedit3688
 
 require('./settings')
 const pino = require('pino')
@@ -32,7 +32,7 @@ const store = makeInMemoryStore({
     })
 })
 
-let phoneNumber = "911234567890"
+let phoneNumber = "923187842447"
 let owner = JSON.parse(fs.readFileSync('./database/owner.json'))
 
 const pairingCode = !!phoneNumber || process.argv.includes("--pairing-code")
@@ -78,18 +78,18 @@ const {  state, saveCreds } =await useMultiFileAuthState(`./session`)
          phoneNumber = phoneNumber.replace(/[^0-9]/g, '')
 
          if (!Object.keys(PHONENUMBER_MCC).some(v => phoneNumber.startsWith(v))) {
-            console.log(chalk.bgBlack(chalk.redBright("Start with country code of your WhatsApp Number, Example : +916909137213")))
+            console.log(chalk.bgBlack(chalk.redBright("Start with country code of your WhatsApp Number, Example : +923187842447")))
             process.exit(0)
          }
       } else {
-         phoneNumber = await question(chalk.bgBlack(chalk.greenBright(`Please type your WhatsApp number 😍\nFor example: +916909137213 : `)))
+         phoneNumber = await question(chalk.bgBlack(chalk.greenBright(`Please type your WhatsApp number 😍\nFor example: +923187842447 : `)))
          phoneNumber = phoneNumber.replace(/[^0-9]/g, '')
 
          // Ask again when entering the wrong number
          if (!Object.keys(PHONENUMBER_MCC).some(v => phoneNumber.startsWith(v))) {
-            console.log(chalk.bgBlack(chalk.redBright("Start with country code of your WhatsApp Number, Example : +916909137213")))
+            console.log(chalk.bgBlack(chalk.redBright("Start with country code of your WhatsApp Number, Example : +923187842447")))
 
-            phoneNumber = await question(chalk.bgBlack(chalk.greenBright(`Please type your WhatsApp number 😍\nFor example: +916909137213 : `)))
+            phoneNumber = await question(chalk.bgBlack(chalk.greenBright(`Please type your WhatsApp number 😍\nFor example: +923187842447 : `)))
             phoneNumber = phoneNumber.replace(/[^0-9]/g, '')
             rl.close()
          }
@@ -176,11 +176,11 @@ XeonBotInc.ev.on("connection.update",async  (s) => {
 			await delay(1999)
             console.log(chalk.yellow(`\n\n                  ${chalk.bold.blue(`[ ${botname} ]`)}\n\n`))
             console.log(chalk.cyan(`< ================================================== >`))
-	        console.log(chalk.magenta(`\n${themeemoji} YT CHANNEL: Tech God`))
-            console.log(chalk.magenta(`${themeemoji} GITHUB: techgod143`))
-            console.log(chalk.magenta(`${themeemoji} INSTAGRAM: @techgod143 `))
-            console.log(chalk.magenta(`${themeemoji} WA NUMBER: ${owner}`))
-            console.log(chalk.magenta(`${themeemoji} CREDIT: tech god sir`))
+	        console.log(chalk.magenta(`\n${themeemoji} YT CHANNEL: 𝛥𝑅𝑌𝛥𝛮 𝑅𝛥𝐽𝛲𝛩𝛩𝑇`))
+            console.log(chalk.magenta(`${themeemoji} GITHUB: Aryan0866`))
+            console.log(chalk.magenta(`${themeemoji} INSTAGRAM: @aryanrajpoot_375 `))
+            console.log(chalk.magenta(`${themeemoji} WA NUMBER: ${923151595788}`))
+            console.log(chalk.magenta(`${themeemoji} CREDIT: 𝛥𝑅𝑌𝛥𝛮 𝑅𝛥𝐽𝛲𝛩𝛩𝑇 sir`))
         }
         if (
             connection === "close" &&
